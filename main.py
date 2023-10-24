f35745647ba535b56215bfb3d213a94839a9dff0
Sebastian Dion <sebastiandion@Sebastians-MacBook-Pro.local>
2023-10-24 16:58:51 -0400

    new main

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+from encode import encode
+from decode import decode
+
 def main():
-    pass
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+    user_input = input('Please enter an option: ')
+    return int(user_input)
+
+password = ''
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        inp = main()
+        if inp == 1:
+            password = input('Please enter your password to encode: ')
+            password = encode(password)
+            print('Your password has been encoded and stored!')
+            print()
+        elif inp == 2:
+            print(f'The encoded password is {password}, and the original password is {decode(password)}.')
+            print()
+        elif inp == 3:
+            exit()
+        else:
+            continue
+
